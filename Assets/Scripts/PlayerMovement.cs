779f79fcc9668bae63586941558f85d41fce5b53
RobertSShoemaker <shoemakerrs@att.net>
2021-10-23 17:02:17 -0500

    -Player sprite now flips depending on movement direction

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -19,17 +19,40 @@ public class PlayerMovement : MonoBehaviour
     void Update()
     {
         Run();
+        FlipSprite();
     }
 
+    //OnMove is the unity created method that takes the keyboard/controller input and moves the player
+    //store the movement input and display it whenever this is called
     void OnMove(InputValue value)
     {
         moveInput = value.Get<Vector2>();
         Debug.Log(moveInput);
     }
 
+    //Change the velocity of the player based on the movement input we got from OnMove()
+    //use runSpeed to control the speed at which the player moves
     void Run()
     {
         Vector2 playerVelocity = new Vector2((moveInput.x * runSpeed), myRigidbody.velocity.y);
         myRigidbody.velocity = playerVelocity;
+    }
+
+    //flip the sprite when the player moves to the left/right
+    void FlipSprite()
+    {
+        //bool is true when the absolute value of the player's x velocity is greater than 0 (or epsilon)
+        //this will keep the player from flipping back to the 'right' side when he isn't moving
+        //this is due to the fact that unity counts 0 as a positive number; meaning that the player 
+        //will always flip to the right when not moving without this code
+        bool playerHasHorizontalSpeed = Mathf.Abs(myRigidbody.velocity.x) > Mathf.Epsilon;
+
+        //player will only flip when actively moving left/right
+        if (playerHasHorizontalSpeed)
+        {
+            //the sprite will face the positive direction when moving on the positve x axis and
+            //will face the negative direction when moving on the negative x axis
+            transform.localScale = new Vector2(Mathf.Sign(myRigidbody.velocity.x), 1f);
+        }
     }
 }
